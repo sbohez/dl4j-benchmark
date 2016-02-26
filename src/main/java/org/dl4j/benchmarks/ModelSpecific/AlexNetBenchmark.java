8c77206ee668b2b6a780b7d1e567a62069cf6b5c
nyghtowl <warrick.melanie@gmail.com>
2016-02-25 19:58:38 -0800

    Added model repo

--- src/main/java/org/dl4j/benchmarks/ModelSpecific/AlexNetBenchmark.java
+++ src/main/java/org/dl4j/benchmarks/ModelSpecific/AlexNetBenchmark.java
@@ -1,5 +1,6 @@
 package org.dl4j.benchmarks.ModelSpecific;
 
+import org.deeplearning4j.AlexNet;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.GradientNormalization;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -32,7 +33,7 @@ public class AlexNetBenchmark {
         int iterations = 1;
         int miniBatchSize = 50;
 
-        MultiLayerNetwork model = AlexNet(height, width, channels, outputNum, 123, iterations);
+        MultiLayerNetwork model = new AlexNet(height, width, channels, outputNum, 123, iterations).init();
 
         int nIterationsBefore = 1;
         int nIterations = 1;
@@ -58,96 +59,96 @@ public class AlexNetBenchmark {
 
     }
 
-    public static MultiLayerNetwork AlexNet(int height, int width, int channels, int outputNum, long seed, int iterations) {
-
-        double nonZeroBias = 1;
-        double dropOut = 0.5;
-        SubsamplingLayer.PoolingType poolingType = SubsamplingLayer.PoolingType.MAX;
-
-        MultiLayerConfiguration.Builder conf = new NeuralNetConfiguration.Builder()
-                .seed(seed)
-                .weightInit(WeightInit.DISTRIBUTION)
-                .dist(new NormalDistribution(0.0, 0.01))
-                .activation("relu")
-                .updater(Updater.NESTEROVS)
-                .iterations(iterations)
-                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer) // normalize to prevent vanishing or exploding gradients
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .learningRate(1e-3)
-                .learningRateScoreBasedDecayRate(1e-1)
-                .regularization(true)
-                .l2(5 * 1e-4)
-                .momentum(0.9)
-                .miniBatch(false)
-                .list()
-                .layer(0, new ConvolutionLayer.Builder(new int[]{11, 11}, new int[]{4, 4}, new int[]{3, 3})
-                        .name("cnn1")
-                        .nIn(channels)
-                        .nOut(96)
-                        .build())
-                .layer(1, new LocalResponseNormalization.Builder()
-                        .name("lrn1")
-                        .build())
-                .layer(2, new SubsamplingLayer.Builder(poolingType, new int[]{3, 3}, new int[]{2, 2})
-                        .name("maxpool1")
-                        .build())
-                .layer(3, new ConvolutionLayer.Builder(new int[]{5, 5}, new int[]{1, 1}, new int[]{2, 2})
-                        .name("cnn2")
-                        .nOut(256)
-                        .biasInit(nonZeroBias)
-                        .build())
-                .layer(4, new LocalResponseNormalization.Builder()
-                        .name("lrn2")
-                        .k(2).n(5).alpha(1e-4).beta(0.75)
-                        .build())
-                .layer(5, new SubsamplingLayer.Builder(poolingType, new int[]{3, 3}, new int[]{2, 2})
-                        .name("maxpool2")
-                        .build())
-                .layer(6, new ConvolutionLayer.Builder(new int[]{3, 3}, new int[]{1, 1}, new int[]{1, 1})
-                        .name("cnn3")
-                        .nOut(384)
-                        .build())
-                .layer(7, new ConvolutionLayer.Builder(new int[]{3, 3}, new int[]{1, 1}, new int[]{1, 1})
-                        .name("cnn4")
-                        .nOut(384)
-                        .biasInit(nonZeroBias)
-                        .build())
-                //conv5
-                .layer(8, new ConvolutionLayer.Builder(new int[]{3, 3}, new int[]{1, 1}, new int[]{1, 1})
-                        .name("cnn5")
-                        .nOut(256)
-                        .biasInit(nonZeroBias)
-                        .build())
-                .layer(9, new SubsamplingLayer.Builder(poolingType, new int[]{3, 3}, new int[]{2, 2})
-                        .name("maxpool3")
-                        .build())
-                .layer(10, new DenseLayer.Builder()
-                        .name("ffn1")
-                        .nOut(4096)
-                        .dist(new GaussianDistribution(0, 0.005))
-                        .biasInit(nonZeroBias)
-                        .dropOut(dropOut)
-                        .build())
-                .layer(11, new DenseLayer.Builder()
-                        .name("ffn2")
-                        .nOut(4096)
-                        .dist(new GaussianDistribution(0, 0.005))
-                        .biasInit(nonZeroBias)
-                        .dropOut(dropOut)
-                        .build())
-                .layer(12, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .name("output")
-                        .nOut(outputNum)
-                        .activation("softmax")
-                        .build())
-                .backprop(true)
-                .pretrain(false)
-                .cnnInputSize(height,width,channels);
-
-        MultiLayerNetwork model = new MultiLayerNetwork(conf.build());
-        model.init();
-
-        return model;
-    }
+//    public static MultiLayerNetwork AlexNet(int height, int width, int channels, int outputNum, long seed, int iterations) {
+//
+//        double nonZeroBias = 1;
+//        double dropOut = 0.5;
+//        SubsamplingLayer.PoolingType poolingType = SubsamplingLayer.PoolingType.MAX;
+//
+//        MultiLayerConfiguration.Builder conf = new NeuralNetConfiguration.Builder()
+//                .seed(seed)
+//                .weightInit(WeightInit.DISTRIBUTION)
+//                .dist(new NormalDistribution(0.0, 0.01))
+//                .activation("relu")
+//                .updater(Updater.NESTEROVS)
+//                .iterations(iterations)
+//                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer) // normalize to prevent vanishing or exploding gradients
+//                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+//                .learningRate(1e-3)
+//                .learningRateScoreBasedDecayRate(1e-1)
+//                .regularization(true)
+//                .l2(5 * 1e-4)
+//                .momentum(0.9)
+//                .miniBatch(false)
+//                .list()
+//                .layer(0, new ConvolutionLayer.Builder(new int[]{11, 11}, new int[]{4, 4}, new int[]{3, 3})
+//                        .name("cnn1")
+//                        .nIn(channels)
+//                        .nOut(96)
+//                        .build())
+//                .layer(1, new LocalResponseNormalization.Builder()
+//                        .name("lrn1")
+//                        .build())
+//                .layer(2, new SubsamplingLayer.Builder(poolingType, new int[]{3, 3}, new int[]{2, 2})
+//                        .name("maxpool1")
+//                        .build())
+//                .layer(3, new ConvolutionLayer.Builder(new int[]{5, 5}, new int[]{1, 1}, new int[]{2, 2})
+//                        .name("cnn2")
+//                        .nOut(256)
+//                        .biasInit(nonZeroBias)
+//                        .build())
+//                .layer(4, new LocalResponseNormalization.Builder()
+//                        .name("lrn2")
+//                        .k(2).n(5).alpha(1e-4).beta(0.75)
+//                        .build())
+//                .layer(5, new SubsamplingLayer.Builder(poolingType, new int[]{3, 3}, new int[]{2, 2})
+//                        .name("maxpool2")
+//                        .build())
+//                .layer(6, new ConvolutionLayer.Builder(new int[]{3, 3}, new int[]{1, 1}, new int[]{1, 1})
+//                        .name("cnn3")
+//                        .nOut(384)
+//                        .build())
+//                .layer(7, new ConvolutionLayer.Builder(new int[]{3, 3}, new int[]{1, 1}, new int[]{1, 1})
+//                        .name("cnn4")
+//                        .nOut(384)
+//                        .biasInit(nonZeroBias)
+//                        .build())
+//                //conv5
+//                .layer(8, new ConvolutionLayer.Builder(new int[]{3, 3}, new int[]{1, 1}, new int[]{1, 1})
+//                        .name("cnn5")
+//                        .nOut(256)
+//                        .biasInit(nonZeroBias)
+//                        .build())
+//                .layer(9, new SubsamplingLayer.Builder(poolingType, new int[]{3, 3}, new int[]{2, 2})
+//                        .name("maxpool3")
+//                        .build())
+//                .layer(10, new DenseLayer.Builder()
+//                        .name("ffn1")
+//                        .nOut(4096)
+//                        .dist(new GaussianDistribution(0, 0.005))
+//                        .biasInit(nonZeroBias)
+//                        .dropOut(dropOut)
+//                        .build())
+//                .layer(11, new DenseLayer.Builder()
+//                        .name("ffn2")
+//                        .nOut(4096)
+//                        .dist(new GaussianDistribution(0, 0.005))
+//                        .biasInit(nonZeroBias)
+//                        .dropOut(dropOut)
+//                        .build())
+//                .layer(12, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+//                        .name("output")
+//                        .nOut(outputNum)
+//                        .activation("softmax")
+//                        .build())
+//                .backprop(true)
+//                .pretrain(false)
+//                .cnnInputSize(height,width,channels);
+//
+//        MultiLayerNetwork model = new MultiLayerNetwork(conf.build());
+//        model.init();
+//
+//        return model;
+//    }
 
 }
